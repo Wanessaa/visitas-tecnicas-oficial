64a99532a02d4f07e04ea50ca195a135ea7c34a0
wanessasantana1972@gmail.com <wanessasantana1972@gmail.com>
2019-03-24 17:05:01 -0300

    organizando o app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,84 +1,77 @@
+//import do angular
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
-import {ButtonModule} from 'primeng/button';
 import{FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import {InputTextModule} from 'primeng/inputtext';
-import {MenuModule} from 'primeng/menu';
 import { RouterModule, Routes } from '@angular/router';
-import {FullCalendarModule} from 'primeng/fullcalendar';
 import {AngularFireModule } from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { configuracao } from 'src/environments/Firebase';
-import { UsuarioComponent } from './cadastrar-usuario/usuario.component';
-import { UsuarioService } from './servicos/usuario.service';
-import { ViagemComponent } from './cadastrar-viagem/viagem.component';
-import { ViagemService } from './servicos/viagem.service';
-import { ListarViagemComponent } from './listar-viagem/listar-viagem.component';
-import { VisualizacaoViagemComponent } from './visualizacao-viagem/visualizacao-viagem.component';
-import { AlterarViagemComponent } from './alterar-viagem/alterar-viagem.component';
-import { TableModule} from 'primeng/table';
-import { AlterarUsuarioComponent } from './alterar-usuario/alterar-usuario.component';
-import { ListarUsuarioComponent } from './listar-usuario/listar-usuario.component';
-import { LoginComponent } from './login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import do primeNg
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {MenuModule} from 'primeng/menu';
+import {FullCalendarModule} from 'primeng/fullcalendar';
+import {TableModule} from 'primeng/table';
 import {BreadcrumbModule} from 'primeng/breadcrumb';
 import {TabMenuModule} from 'primeng/tabmenu';
-import { CadastrarCoordenadorComponent } from './cadastrar-coordenador/cadastrar-coordenador.component';
-import { CoordenadorService } from './servicos/coordenador.service';
-import { MenuAdmComponent } from './menu-adm/menu-adm.component';
 import {InputTextareaModule} from 'primeng/inputtextarea';
-
-
-
-//import {MessageService} from 'primeng//api';
-import { CadastrarSetorComponent } from './cadastrar-setor/cadastrar-setor.component';
+import {ToastModule} from 'primeng/toast';
 import {MenubarModule} from 'primeng/menubar';
-import {MenuItem, MessageService} from 'primeng/api';
+import {MessageService} from 'primeng/api';
 import {SpinnerModule} from 'primeng/spinner';
 import {CalendarModule} from 'primeng/calendar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ToastModule} from 'primeng/toast';
-import { MessageServiceDemo } from '../message/message';
+import {DropdownModule} from 'primeng/dropdown';
+//components
+import { AlterarSetorComponent } from './alterar-setor/alterar-setor.component';
+import { AlterarUsuarioComponent } from './alterar-usuario/alterar-usuario.component';
+import { AlterarViagemComponent } from './alterar-viagem/alterar-viagem.component';
+import { AlterarParecerComponent } from './alterar-parecer/alterar-parecer.component';
+import { CadastrarSetorComponent } from './cadastrar-setor/cadastrar-setor.component';
+import { CadastrarCoordenadorComponent } from './cadastrar-coordenador/cadastrar-coordenador.component';
 import { ListarCoordenadorComponent } from './listar-coordenador/listar-coordenador.component';
+import { ListarSetorComponent } from './listar-setor/listar-setor.component';
+import { ListarParecerComponent } from './listar-parecer/listar-parecer.component';
+import { ListarViagemComponent } from './listar-viagem/listar-viagem.component';
+import { ListarUsuarioComponent } from './listar-usuario/listar-usuario.component';
+import { ListagemParaCoordenadorComponent } from './listar-usuarios-por-setor/listagem-para-coordenador.component';
+import { LoginComponent } from './login/login.component';
 import { MenuCoordenadorComponent } from './menu-coordenador/menu-coordenador.component';
 import { MenuAdministradorComponent } from './menu-administrador/menu-administrador.component';
 import { MenuUsuarioComponent } from './menu-usuario/menu-usuario.component';
-//import { Message } from '@angular/compiler/src/i18n/i18n_ast';
-import {DropdownModule} from 'primeng/dropdown';
-import { ListarSetorComponent } from './listar-setor/listar-setor.component';
-import { AlterarSetorComponent } from './alterar-setor/alterar-setor.component';
+import { UsuarioComponent } from './cadastrar-usuario/usuario.component';
+import { ViagemComponent } from './cadastrar-viagem/viagem.component';
+import { VisualizacaoViagemComponent } from './visualizacao-viagem/visualizacao-viagem.component';
+//servicos
+import { UsuarioService } from './servicos/usuario.service';
+import { ViagemService } from './servicos/viagem.service';
 import { ParecerService } from './servicos/parecer.service';
-import { ListarParecerComponent } from './listar-parecer/listar-parecer.component';
-import { AlterarParecerComponent } from './alterar-parecer/alterar-parecer.component';
-import { ListagemParaCoordenadorComponent } from './listar-usuarios-por-setor/listagem-para-coordenador.component';
-// import { VisitaComponent } from './listar/visita/visita.component';
-// import { ListarVisitaComponent } from './listar-visita/listar-visita.component';
-
-
-
+import { CoordenadorService } from './servicos/coordenador.service';
 
 
 
 export const rotas : Routes = [ 
+  {path: "", component:LoginComponent},
+  {path: "administrador/menu", component:MenuAdministradorComponent},
+  {path: "administrador/listarTodosUsuarios", component:ListarUsuarioComponent},
+  {path: "coordenador/menu", component:MenuCoordenadorComponent},
+  {path: "coordenador/cadastro", component:CadastrarCoordenadorComponent},
+  {path: "coordenador/ListagemUsuarios", component:ListagemParaCoordenadorComponent},
+  {path: "coordenador/Parecer/Listar", component:ListarParecerComponent},
+  {path: "coordenador/Parecer/atualizar/:id", component:AlterarParecerComponent},
+  {path: "setor/listar", component:ListarSetorComponent},
+  {path: "setor/visualizacao/:id", component:VisualizacaoViagemComponent},
+  {path: "setor/atualizar/:id", component:AlterarSetorComponent},
+  {path: "setor/cadastro", component:CadastrarSetorComponent},
+  {path: "usuario/menu", component:MenuUsuarioComponent},
+  {path: "usuario/atualizar/:id", component:AlterarUsuarioComponent},
+  {path: "visita/atualizar/:id", component:AlterarViagemComponent},
+  {path: "visita/visualizacao/:id", component:VisualizacaoViagemComponent},
   {path: "usuario/cadastro", component:UsuarioComponent},
   {path: "visita/cadastro", component:ViagemComponent},
   {path: "visita/listar/:id", component:ListarViagemComponent},
-  {path: "administrador/listarTodosUsuarios", component:ListarUsuarioComponent},
-  {path: "setor/listar", component:ListarSetorComponent},
-  {path: "visita/visualizacao/:id", component:VisualizacaoViagemComponent},
-  {path: "setor/visualizacao/:id", component:VisualizacaoViagemComponent},
-  {path: "", component:LoginComponent},
-  {path: "usuario/menu", component:MenuUsuarioComponent},
-  {path: "coordenador/menu", component:MenuCoordenadorComponent},
-  {path: "administrador/menu", component:MenuAdministradorComponent},
-  {path: "visita/atualizar/:id", component:AlterarViagemComponent},
-  {path: "usuario/atualizar/:id", component:AlterarUsuarioComponent},
-  {path: "setor/atualizar/:id", component:AlterarSetorComponent},
-  {path: "coordenador/cadastro", component:CadastrarCoordenadorComponent},
-  {path: "setor/cadastro", component:CadastrarSetorComponent},
-  {path: "coordenador/ListagemUsuarios", component:ListagemParaCoordenadorComponent},
-  {path: "coordenador/Parecer/Listar", component:ListarParecerComponent},
-  {path: "coordenador/Parecer/atualizar/:id", component:AlterarParecerComponent}
+  
  
 
 
@@ -87,43 +80,31 @@ export const rotas : Routes = [
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ViagemComponent,
     UsuarioComponent,
-    ViagemComponent,
-    ListarViagemComponent,
-    VisualizacaoViagemComponent,
+    AppComponent,
+    AlterarSetorComponent,
+    AlterarParecerComponent,
     AlterarViagemComponent,
     AlterarUsuarioComponent,
-    ListarUsuarioComponent,
-    LoginComponent,
     CadastrarSetorComponent,
     CadastrarCoordenadorComponent,
-    MenuAdmComponent,
+    ListarViagemComponent,
+    ListagemParaCoordenadorComponent,
+    ListarSetorComponent,
+    ListarUsuarioComponent,
     ListarCoordenadorComponent,
+    ListarParecerComponent,
+    ListarViagemComponent,
+    LoginComponent,
+    ViagemComponent,
+    VisualizacaoViagemComponent,
     MenuCoordenadorComponent,
     MenuAdministradorComponent,
     MenuUsuarioComponent,
-    ListagemParaCoordenadorComponent,
-    ListarSetorComponent,
-    AlterarSetorComponent,
-
-    ListarParecerComponent,
-    AlterarParecerComponent,
-    
-   
     
     // VisitaComponent,
     // ListarVisitaComponent,
     
-
-    
-   
-   
-  
-    
-    
-
   
     
   ],
@@ -145,9 +126,6 @@ export const rotas : Routes = [
     BrowserAnimationsModule,
     ToastModule,
     DropdownModule,
-    // MessageServiceDemo,
-      //MessageService,
-    //Message
     InputTextareaModule
     
     
